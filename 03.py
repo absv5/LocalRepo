cbfe7148bca95d9e5239125d00e15db0a67ee52e
absv5 <abhometv5@gmail.com>
2025-04-18 12:40:30 +0530

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import socket
 a=socket.gethostbyname('youtube.com')
 print(a)
-=======
-print('new 3rd file')
-print('for feature1 yes')
->>>>>>> 0c337c69394ac612613f857c5a34b9838100a78c
+
